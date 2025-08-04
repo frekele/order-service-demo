3a8540636ebf2f01056f3051021635afc82ca75a
Leandro Kersting de Freitas <leandro@frekele.org>
2025-08-04 00:13:02 -0300

    feat: implements interface OrderGateway.

--- domain/src/main/java/br/com/demo/domain/gateway/OrderGateway.java
+++ domain/src/main/java/br/com/demo/domain/gateway/OrderGateway.java
@@ -1,6 +1,16 @@
 package br.com.demo.domain.gateway;
 
+import br.com.demo.domain.model.Order;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface OrderGateway {
 
+    Optional<Order> findById(UUID id);
+
+    Optional<Order> findByExternalOrderId(String externalOrderId);
+
+    Order save(Order order);
 
 }
